6abecd5961383a1d9801a763179e3bab8875e7e0
willowsenator <willowsenator@gmail.com>
2023-06-09 22:41:05 +0200

    Build transaction, sign and send

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import warnings
-from datetime import datetime, timezone
-from web3 import Web3
-import pandas as pd
 import json
-import time
 
-warnings.simplefilter(action="ignore", category=FutureWarning)
+from web3 import Web3
 
 # Contract address and ABI
 contract_address = "0x0E3A8078EDD2021dadcdE733C6b4a86E51EE8f07"
@@ -15,6 +10,8 @@ with open("abi.json") as abi_file:
 abi = json.loads(data)
 
 # Provider
+account_mainnet = ""
+pk_account = ""
 rpc_mainnet = "https://bsc-dataseed.binance.org"
 w3 = Web3(Web3.HTTPProvider(rpc_mainnet))
 
@@ -24,64 +21,40 @@ contract = w3.eth.contract(address=contract_address, abi=abi)
 # Current epoch
 current_epoch = contract.functions.currentEpoch().call()
 
-lookback = 70
-starting_epoch = current_epoch - lookback
-columns = ["epoch", "start_timestamp", "lock_timestamp", "close_timestamp", "lock_price", "close_price",
-           "total_amount", "bull_amount", "bear_amount", "bull_ratio", "bear_ratio", "oracle_called"]
-df = pd.DataFrame(columns=columns)
 
-count = 0
-for e in range(0, lookback):
-    time.sleep(1)
-    starting_epoch += 1
-    count += 1
-    print(starting_epoch, count)
+# Send tx
+def send_tx(side):
+    chain_id = 56
+    gas = 300000
+    gas_price = Web3.to_wei("5.5", "gwei")
+    send_bnb = 0.001
+    amount = Web3.to_wei(send_bnb, "ether")
 
-    current_rounds_list = contract.functions.rounds(starting_epoch).call()
+    # Nonce
+    nonce = w3.eth.get_transaction_count(account_mainnet)
 
-    # Name items
-    epoch = current_rounds_list[0]
-    start_timestamp = current_rounds_list[1]
-    lock_timestamp = current_rounds_list[2]
-    close_timestamp = current_rounds_list[3]
-    lock_price = current_rounds_list[4]
-    close_price = current_rounds_list[5]
-    total_amount = current_rounds_list[8]
-    bull_amount = current_rounds_list[9]
-    bear_amount = current_rounds_list[10]
-    oracle_called = current_rounds_list[13]
+    # Build Tx BULL
+    if side == "bull":
+        tx_build = contract.functions.betBull(current_epoch, amount).buildTransaction({
+            "chainId": chain_id,
+            "gas": gas,
+            "value": amount,
+            "gasPrice": gas_price,
+            "nonce": nonce
+        })
 
-    # Calculate ratio
-    total_amount_in_human = round(float(Web3.from_wei(total_amount, "ether")), 5)
-    bear_amount_in_human = round(float(Web3.from_wei(bear_amount, "ether")), 5)
-    bull_amount_in_human = round(float(Web3.from_wei(bull_amount, "ether")), 5)
-
-    # Ratios
-    if bear_amount_in_human != 0 and bull_amount_in_human != 0:
-        bull_ratio = round(bull_amount_in_human / bear_amount_in_human, 2)
-        bear_ratio = round(bear_amount_in_human / bull_amount_in_human, 2)
+    # Build Tx BEAR
     else:
-        bull_ratio = 0
-        bear_ratio = 0
+        tx_build = contract.functions.betBear(current_epoch, amount).buildTransaction({
+            "chainId": chain_id,
+            "gas": gas,
+            "value": amount,
+            "gasPrice": gas_price,
+            "nonce": nonce
+        })
 
-    row_dict = {
-        "epoch": epoch,
-        "start_timestamp": start_timestamp,
-        "lock_timestamp": lock_timestamp,
-        "close_timestamp": close_timestamp,
-        "lock_price": lock_price,
-        "close_price": close_price,
-        "total_amount": total_amount_in_human,
-        "bull_amount": bull_amount_in_human,
-        "bear_amount": bear_amount_in_human,
-        "bull_ratio": bull_ratio,
-        "bear_ratio": bear_ratio,
-        "oracle_called": oracle_called,
-    }
+    # Sign Tx
+    tx_signed = w3.eth.account.sign_transaction(tx_build, pk_account)
 
-    try:
-        df.loc[len(df)] = row_dict
-        df.to_csv("predictions.csv", index=False)
-    except Exception as e:
-        print("Error occurred:", str(e))
-
+    # Send Tx
+    sent_tx = w3.eth.send_raw_transaction(tx_signed.rawTransaction)
